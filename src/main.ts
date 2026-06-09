8708d8527b3ac4b67eb775ce94155628d01d538a
Sebastien Dubois <seb@dsebastien.net>
2026-06-09 22:22:55 +0200

    feat(engine): wire menu, game and game-over scenes into the engine bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { Color, DisplayMode, Engine } from 'excalibur';
+import { COLOR_BACKGROUND, GAME_HEIGHT, GAME_WIDTH } from './constants';
+import { GameOverScene } from './scenes/gameover';
+import { GameScene } from './scenes/game';
+import { MenuScene } from './scenes/menu';
 
 const game = new Engine({
-  width: 800,
-  height: 800,
+  width: GAME_WIDTH,
+  height: GAME_HEIGHT,
   displayMode: DisplayMode.FitScreen,
-  backgroundColor: Color.fromHex('#0b0e1d'),
+  backgroundColor: Color.fromHex(COLOR_BACKGROUND),
   canvasElementId: 'game',
   suppressPlayButton: true,
 });
 
-void game.start();
+game.add('menu', new MenuScene());
+game.add('game', new GameScene());
+game.add('gameover', new GameOverScene());
+
+void game.start().then(() => game.goToScene('menu'));
